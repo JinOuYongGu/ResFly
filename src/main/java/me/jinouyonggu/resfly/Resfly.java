50a203e643ac2e016e767fd2a8dc09dfe4bc5289
Jin_ou <691422759@qq.com>
2021-05-15 15:03:54 +0800

    Initial commit

---
+++ src/main/java/me/jinouyonggu/resfly/Resfly.java
@@ -0,0 +1,17 @@
+package me.jinouyonggu.resfly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Resfly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
